d923bbb0fc351f139f092274c7671c2d20e9f0b4
jeyni02 <jeynilomary@uam.edu.ni>
2026-05-20 21:41:51 -0600

    feat: create pet detail screen

---
+++ app/src/main/java/ni/edu/uam/petcare/ui/screens/PetDetailScreen.kt
@@ -0,0 +1,32 @@
+package ni.edu.uam.petcare.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@Composable
+fun PetDetailScreen(navController: NavController) {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(text = "Detalle de Mascota")
+
+        Button(
+            onClick = {
+                navController.navigate("edit_pet")
+            }
+        ) {
+            Text("Editar")
+        }
+    }
+}

c31aec00511b0ce728324db6f706ceffea78affa
Arelys Castillo <abrigitteo@uamv.edu.ni>
2026-05-20 22:31:29 -0600

    feat: create empty state component

---
+++ app/src/main/java/ni/edu/uam/petcare/ui/components/EmptyState.kt
@@ -0,0 +1,31 @@
+package ni.edu.uam.petcare.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun EmptyState(message: String) {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(
+            text = "🐾",
+            fontSize = 60.sp
+        )
+
+        Text(
+            text = message,
+            fontSize = 20.sp
+        )
+    }
+}
